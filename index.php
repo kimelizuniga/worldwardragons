1fb0d55c696581fff6d1468f1185024b4badf557
Kim Zuniga <kim.eli.zuniga@gmail.com>
2022-04-26 09:48:46 -0400

    Disable saving cache

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
 -->
 <html>
 <head>
+<?php 
+	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+	header('Cache-Control: no-store, no-cache, must-revalidate');
+	header('Cache-Control: post-check=0, pre-check=0', FALSE);
+	header('Pragma: no-cache');
+?>
 <meta charset="UTF-8">
 <meta name="authoring-tool" content="Adobe_Animate_CC">
 <title>World War Dragons</title>
