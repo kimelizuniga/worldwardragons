7a1ee8ff3eeec533d18c4b07f5935f691ef17955
kzuniga <kimeli.zuniga@student.sl.on.ca>
2023-07-02 19:58:06 -0400

    Change connection to cleardb

--- clsSQLConnection.php
+++ clsSQLConnection.php
@@ -18,7 +18,7 @@ class clsSQLConnection
         $query_builder = TRUE;
     
         // Connect to DB
-        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     }
     else
     {
